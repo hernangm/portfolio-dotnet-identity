cd4a0b24ae6bd1fb08599d970dc3882d35944552
Hernan Marano <herchugm@gmail.com>
2025-10-15 13:17:04 -0300

    more changes

--- src/Portfolio.Dotnet.Identity.Data/DatabaseConfigurationUtilities.cs
+++ src/Portfolio.Dotnet.Identity.Data/DatabaseConfigurationUtilities.cs
@@ -5,21 +5,11 @@ namespace Portfolio.Dotnet.Identity.Data
 {
     public static class DatabaseConfigurationUtilities
     {
-        //public static Action<IServiceProvider, DbContextOptionsBuilder> ConfigureDbContextOptionsBuilder(string connectionString, bool isProduction)
-        //{
-        //    void b(IServiceProvider provider, DbContextOptionsBuilder builder)
-        //    {
-        //        builder.ConfigureIdentityDataContext(connectionString, isProduction);
-        //    }
-        //    return b;
-        //}
-
         public static DbContextOptionsBuilder ConfigureIdentityDataContext(this DbContextOptionsBuilder builder, string connectionString, bool isProduction)
         {
             builder.UseNpgsql(connectionString, sqlOptions =>
             {
                 sqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
-                //sqlOptions.MigrationsAssembly(assemblyName);
                 sqlOptions.EnableRetryOnFailure();
             });
             builder.ConfigureWarnings(w =>
